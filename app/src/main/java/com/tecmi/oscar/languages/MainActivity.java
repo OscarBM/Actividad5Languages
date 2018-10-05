4ba01939bb7a994f5b4ec2dc0b9af9644c894683
Oscar <oscarismaelbm@gmail.com>
2018-10-04 22:50:40 -0500

    Initial commit

---
+++ app/src/main/java/com/tecmi/oscar/languages/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tecmi.oscar.languages;
+//Hecho por Oscar Beltràn 2701703
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+//API KEY: bfa11dfaa995caaedd3d1ddb36e6813c
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
